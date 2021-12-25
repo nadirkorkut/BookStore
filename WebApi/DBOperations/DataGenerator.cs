0865d75ba837e365d5aa3d26410c3cb98458ffe4
nadirkorkut <korkutnadir@outlook.com>
2021-12-25 19:31:01 +0300

    .

---
+++ WebApi/DBOperations/DataGenerator.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApi.DBOperations
+{
+    public class DataGenerator
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BookStoreDbContext(serviceProvider.GetRequiredService<DbContextOptions<BookStoreDbContext>>()))
+            {
+                if(context.Books.Any())
+                {
+                    return;
+                }
+
+                context.Books.AddRange(
+                     new Book
+                    {
+                    //Id = 1,
+                    Title = "Leon Startup",
+                    GenreId = 1, // Personal Growth
+                    PageCount = 200,
+                    publishDate = new DateTime(2001,06,12)
+                    },
+                    new Book
+                    {
+                    //Id = 2,
+                    Title = "Herland",
+                    GenreId = 2, // Science Fiction
+                    PageCount = 250,
+                    publishDate = new DateTime(2010,05,23)
+                    },
+                    new Book
+                    {
+                    //Id = 3,
+                    Title = "Dune",
+                    GenreId = 3, // Science Fiction
+                    PageCount = 540,
+                    publishDate = new DateTime(2002,12,21)
+                    }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
